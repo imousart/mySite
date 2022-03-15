4869c4819edf234c6f28794118dd9c78be1303b5
imousart <mousa@jrafedia.sa>
2022-03-15 04:42:24 +0300

    add splitbee cdn

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="flex justify-center mt-10 font-ibm-200 text-indigo-600 ">
+    <h1 class="flex justify-center mt-10 font-ibm-200 text-purple-700 ">
       {{ message }}
     </h1>
   </div>
@@ -8,6 +8,7 @@
 
 <script>
 export default {
+  name: 'IndexPage',
   data () {
     return {
       message: ':)في المرة القادة لزيارتك سوف نقوم بإضافة بعض المزيد من المعلومات عن الموقع'
