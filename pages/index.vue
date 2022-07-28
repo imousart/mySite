7a62ed361d624cff3a4c5bf0a22a93add743dc49
imousart <mousa@jrafedia.sa>
2022-07-28 04:13:30 +0300

    !

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <h1 class="flex justify-center mt-10 font-ibm-200 text-purple-700 ">
+  <div class="flex items-center justify-center w-screen h-screen p-5 font-">
+    <div class=" flex items-">
+      <div>
+        <img src="./assets/" alt="">
+      </div>
+    <h1 class="text-justify text-purple-700 ">
       {{ message }}
     </h1>
+    </div>
   </div>
 </template>
 
@@ -11,7 +16,17 @@ export default {
   name: 'IndexPage',
   data () {
     return {
-      message: ':)في المرة القادة لزيارتك سوف نقوم بإضافة بعض المزيد من المعلومات عن الموقع'
+      message: 'قريباً أشياء جميلة تحدث هنا ',
+      users: [
+        {
+          id: 1,
+          name: 'imousart'
+        },
+        {
+          id: 2,
+          name: 'ifalcondes'
+        }
+      ]
     }
   },
   head () {
